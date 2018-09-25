ccfa91d288c26e3152d9eadf7e8a9514d1c82587
Jasmine Banares <jasminebanares@dyn-118-139-47-154.its.monash.edu.au>
2018-09-25 18:48:03 +1000

    added assignment files

--- 2-assignment/skeleton/js/formView.js
+++ 2-assignment/skeleton/js/formView.js
@@ -1,1 +1,93 @@
 "use strict";
+/**
+ * TO FIX:
+ *  - form prompts are also cleared when called,
+ *  - form doesnt clear when address is inputted
+ *  - needs proper documentation
+ *  - make code neater and more readable
+ *  - add error return codes
+ *  - validate form answers properly
+ */
+
+/** FEATURE 3 */
+function clearForm(){
+    // input fields to be cleared
+    let address = document.getElementById('address');
+    let roomNumber = document.getElementById('roomNumber');
+    let seatsUsed = document.getElementById('seatsUsed');
+    let seatsTotal = document.getElementById('seatsTotal');
+    
+    // make a list to prep for iteration
+    let formElements = [address, roomNumber, seatsUsed, seatsTotal];
+    
+    // loop through input fields and clear values
+    for (let i = 0; i < formElements.length; ++i) {
+        formElements[i].value = '';
+    }
+}
+
+function saveForm(){
+    // get elements
+    let id = ['roomNumber', 'address', 'lights', 'heatingCooling', 'seatsUsed', 'seatsTotal'];
+    id = id.map(i => document.getElementById(i));
+
+    // make new instance
+    let roomData = new RoomUsage(parseInt(id[0]).value, id[1].value, id[2].checked, id[3].checked, parseInt(id[4].value), parseInt(id[4].value), new Date());
+
+    // test to see if instance hold right data
+    localStorage.setItem('roomDataTest', JSON.stringify(roomData));
+    /** THIS CODE DOES NOT CHECK IF ALL INPUTS ARE VALID, WILL ADD THAT LATER ON FOR NOW THIS CREATES THE INSTANCES */
+}
+
+/** FEATURE 4 */
+// callback functions
+function success(pos){
+    // returns latitude and longuitude
+    let lat = pos.coords.latitude;
+    let lng = pos.coords.longitude;
+
+    // debugging purposes
+    console.log(lat, lng)
+
+    // get data using API
+    requestUrl(lat, lng);
+}
+
+function error(err){
+    // https://developer.mozilla.org/en-US/docs/Web/API/PositionError 
+    // info on the different error codes
+    return err.code;
+}
+
+// get current address
+function getAddress(){
+    // check if box is ticked
+    let checkbox = document.getElementById('useAddress');
+
+    if (checkbox.checked){
+        if (!navigator.geolocation){
+            // if geolocation not available, return 1
+            // CHANGE RETURN NUMBER SINCE ERROR FUNCTION RETURNS 1 AS WELL
+            return 1;
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+function requestUrl(lat, lng){
+    let url = 'https://api.opencagedata.com/geocode/v1/json?q='
+        + lat + '+' + lng + '&key=6bac0036676840c39c653d8cc4514459&no_annotations=1&callback=addressResponse';
+    
+    let script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+}
+
+function addressResponse(addressArray){
+    // get formatted address to put into form
+    let data = addressArray.results[0].formatted;
+
+    // put into form
+    document.getElementById('address').value = data;
+}
--- 2-assignment/skeleton/js/shared.js
+++ 2-assignment/skeleton/js/shared.js
@@ -1,1 +1,110 @@
 "use strict";
+class RoomUsage
+    {
+       constructor(roomNumber, address,lightsOn,heatingCoolingOn,seatsUsed,seatsTotal,timeChecked) 
+         {
+           this._roomNumber = roomNumber;
+           this._address = address;
+           this._lightsOn = lightsOn;
+           this._heatingCoolingOn = heatingCoolingOn;
+           this._seatsUsed = seatsUsed;
+           this._seatsTotal = seatsTotal;
+           this._timeChecked = timeChecked;   
+         }
+        
+     //roomNumber   
+    get roomNumber()
+        {
+            return this._roomNumber;
+        }
+    set roomNumber(newroomNumber)
+        {
+            if(newroomNumber != "")
+                {
+                    this._roomNumber = newroomNumber;
+                }
+        }
+    //address
+    get address()
+        {
+            return this._address;
+        }
+    set address(newaddress)
+        {
+            if(newaddress != "")
+                {
+                    this._address = newaddress;
+                }
+        }
+    //lightsOn
+    get lightsOn()
+        {
+            return this._lightsOn;
+        }
+    set lightsOn(newlightsOn)
+        {
+            if(newlightsOn == true || newlightsOn == false)
+                {
+                    this._address = newaddress;
+                }
+        }
+    //heatingCoolingOn
+    get heatingCoolingOn()
+        {
+            return this._heatingCoolingOn;
+        }
+    set heatingCoolingOn(newheatingCoolingOn)
+        {
+            if(newheatingCoolingOn == true || newheatingCoolingOn == false)
+                {
+                  this._heatingCoolingOn = newheatingCoolingOn;
+                }
+        }
+       //seatsTotal 
+        get seatsTotal()
+        {
+            return this._seatsTotal;
+        }
+    set seatsTotal(newseatsTotal)
+        {
+            if(newseatsTotal > 0)
+                {
+                    this._seatsTotal = newseatsTotal;
+                }
+        }
+        
+        
+        
+    //seatsUsed
+        get seatsUsed()
+        {
+            return this._seatsUsed;
+        }
+    set seatsUsed(newseatsUsed)
+        {
+            if(newseatsUsed >= 0 || newseatsUsed <= seatsTotal)
+                {
+                    this._seatsUsed = seatsUsed;
+                }
+        }     
+    }
+
+
+class RoomUsageList
+    {
+        construct(RoomUsageList)
+        {
+           this._RoomUsageList = []; 
+        }
+        
+        adding(roomname, usage)
+        {
+            let usagelist = {
+                room:roomname,
+                usage:usage 
+            }
+            this._RoomUsageList.push(usagelist);
+            console.log("Roomname" + roomname + ", usage" + usage);    
+        }
+    
+    }
